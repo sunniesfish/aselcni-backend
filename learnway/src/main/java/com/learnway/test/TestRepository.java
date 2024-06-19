798cfe23aad58007ad6e820ae52a8d9cce4f2488
sunniesfish <hhch5611.jh@gmail.com>
2024-06-19 17:45:48 +0900

    aws test 용

--- learnway/src/main/java/com/learnway/test/TestRepository.java
+++ learnway/src/main/java/com/learnway/test/TestRepository.java
@@ -1,2 +1,8 @@
-package com.learnway.test;public interface TestRepository {
+package com.learnway.test;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestRepository extends JpaRepository<Test, Long> {
 }
